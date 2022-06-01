84125594f8dec9bde7d64dd5ab29fb59c06fa3e4
SHAIK FAYAZ <79346748+Fayaz-shaik@users.noreply.github.com>
2022-06-01 21:34:49 +0530

    Added solution - LeetHub

---
+++ 5. Max and Second Max - GFG/5.-max-and-second-max.cpp
@@ -0,0 +1,75 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+// CPP code to find largest and 
+// second largest element in the array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+     
+
+class Solution{
+  public:
+    /* Function to find largest and second largest element
+    *sizeOfArray : number of elements in the array
+    * arr = input array
+    */
+    vector<int> largestAndSecondLargest(int sizeOfArray, int arr[]){
+        int max = INT_MIN, max2= INT_MIN;
+        
+        /*********************************
+         * Your code here
+         * This function should return a
+         * vector with first element as
+         * max and second element as 
+         * second max
+         * *******************************/
+         set <int> mp;
+         vector <int> vi;
+         for(int i=0;i<sizeOfArray;i++){
+             mp.insert(arr[i]);
+         }
+         if(mp.size()==1) {
+             auto it1 = mp.begin();
+             vi.push_back(*(it1++));
+             vi.push_back(-1);
+             return vi;
+         }
+         auto it = mp.end();
+         vi.push_back(*(--it));
+         vi.push_back(*(--it));
+         return vi;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main() {
+	
+	// Testcase input
+	int testcases;
+	cin >> testcases;
+    
+    // Looping through all testcases
+	while(testcases--){
+	    int sizeOfArray;
+	    cin >> sizeOfArray;
+	    
+	    int arr[sizeOfArray];
+	    
+	    // Array input
+	    for(int index = 0; index < sizeOfArray; index++){
+	        cin >> arr[index];
+	    }
+	    Solution obj;
+	    vector<int> ans = obj.largestAndSecondLargest(sizeOfArray, arr);
+	    cout<<ans[0]<<' '<<ans[1]<<endl;
+	}
+	
+	return 0;
+}  // } Driver Code Ends

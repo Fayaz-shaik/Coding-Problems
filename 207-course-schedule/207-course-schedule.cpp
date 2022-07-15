b86e7cbb4647f5e6ebbb0e0a92ddc618bb3a1894
SHAIK FAYAZ <79346748+Fayaz-shaik@users.noreply.github.com>
2022-07-15 18:51:40 +0530

    Time: 42 ms (32.21%), Space: 14.4 MB (38.10%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool cyc(int k , vector<int> adj[],vector<int> &vis){
+        if(vis[k]==1){
+            return true;
+        }
+        if(vis[k]==0){
+            vis[k]=1;
+            for(auto it : adj[k]){
+                if(cyc(it,adj,vis))
+                    return true;
+            }
+        }
+        vis[k]=2;
+        return false;
+    }
+    bool canFinish(int num, vector<vector<int>>& pre) {
+        vector<int> adj[num];
+        bool ans = true;
+        for(auto i :pre){
+            adj[i[1]].push_back(i[0]);            
+        }
+        vector<int> vis(num,0);
+        for(int k=0;k<num;k++){
+            if(cyc(k,adj,vis)==true){
+                ans = false ;
+            }
+        }
+        return ans;
+    }
+};
